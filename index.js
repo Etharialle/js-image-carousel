634a29b5c95c0ea677c89eacaedbec91c2ef330f
etharialle <christopher.m.adams2@gmail.com>
2024-05-23 18:37:02 -0700

    feat: rightArrow function added

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ imageArray = [
 ]
 
 const frame = document.querySelector("#frame");
-const firstImage = document.createElement("img");
-firstImage.src = imageArray[0];
-frame.appendChild(firstImage);
+let frameImage = document.createElement("img");
+frameImage.src = imageArray[0];
+frame.appendChild(frameImage);
+
+let framePosition = 0;
+
+const rightArrow = document.querySelector("#right");
+rightArrow.addEventListener("click", () => {
+    if (framePosition === (imageArray.length - 1)) {
+        framePosition = 0;
+    } else {
+        framePosition++;
+    }
+    frameImage.src = imageArray[framePosition];
+})
